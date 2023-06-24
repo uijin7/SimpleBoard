4b3c11ec7e40f110462ddf3a1c3a33a29356638d
uijin7 <hwanguijin7@gmail.com>
2023-06-24 23:04:31 +0900

    스웨거 Config 추가

---
+++ src/main/java/com/example/simpleboard/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.example.simpleboard.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("API 타이틀")
+                .description("API Description");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
