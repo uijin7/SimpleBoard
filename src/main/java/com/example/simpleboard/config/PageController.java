3429a9d3e23cf5fa779145a41ba4f30d9011d382
uijin7 <hwanguijin7@gmail.com>
2026-03-23 23:11:59 +0900

    게시판 수정 구현 및 삭제 버튼 생성

--- src/main/java/com/example/simpleboard/config/PageController.java
+++ src/main/java/com/example/simpleboard/config/PageController.java
@@ -1,7 +1,9 @@
 package com.example.simpleboard.config;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
@@ -14,5 +16,11 @@ public class PageController {
     @GetMapping("/posts/write")
     public String postWritePage() {
         return "post-write";
+    }
+    
+    @GetMapping("/posts/edit/{id}")
+    public String postEditPage(@PathVariable Long id, Model model) {
+        model.addAttribute("postId", id);
+        return "post-edit";
     }
 }
